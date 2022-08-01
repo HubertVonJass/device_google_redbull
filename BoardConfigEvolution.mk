16bdd4e86273809e873ef6e7e2a9af738c8041b9
Hugh <szyzygy123@gmail.com>
2022-08-01 09:27:55 -0400

    adding device_google_redbull

---
+++ BoardConfigEvolution.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1
+TARGET_KERNEL_CONFIG := redbull_defconfig
+TARGET_NEEDS_DTBOIMAGE := true
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+
+# Kernel modules
+BOOT_KERNEL_MODULES += \
+    heatmap.ko \
+    touch_offload.ko
+
+KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/redbull/modules.load))
+KERNEL_MODULES_LOAD := $(foreach m,$(KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(filter-out $(BOOT_KERNEL_MODULES), $(KERNEL_MODULES_LOAD))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(filter $(BOOT_KERNEL_MODULES), $(KERNEL_MODULES_LOAD))
+
+# Manifests
+DEVICE_MANIFEST_FILE += device/google/redbull/lineage_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/evolution/config/device_framework_matrix.xml
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor
+
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Reserve space for gapps install
+ifneq ($(WITH_GMS),true)
+BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 614400000
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1752350720
+endif
+BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 30720000
+BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 30720000
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/dynamic
+BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/vendor
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
